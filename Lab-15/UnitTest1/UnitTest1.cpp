07f6ad9f5bf23446a4403c725e524dfb7c02f24e
Mr_ShadowKnight_ <volodymyr.pinchak@gmail.com>
2024-11-20 21:30:21 +0200

    Add.index

---
+++ Lab-15/UnitTest1/UnitTest1.cpp
@@ -0,0 +1,66 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Lab-15/Lab-15.cpp"
+
+using namespace std;
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTestNQueens
+{
+	TEST_CLASS(UnitTestNQueens)
+	{
+	public:
+		TEST_METHOD(TestPrintSolution)
+		{
+			int n = 4;
+			position[0] = 1; // Встановлюємо рішення
+			position[1] = 3;
+			position[2] = 0;
+			position[3] = 2;
+
+			// Захоплення вихідних даних
+			ostringstream outputBuffer;
+			streambuf* oldCoutBuffer = cout.rdbuf(outputBuffer.rdbuf());
+
+			printSolution(n);
+
+			cout.rdbuf(oldCoutBuffer); // Відновлення початкового буфера
+
+			// Очікуваний вивід
+			string expectedOutput =
+				". Q . . \n"
+				". . . Q \n"
+				"Q . . . \n"
+				". . Q . \n\n";
+
+			Assert::AreEqual(expectedOutput, outputBuffer.str());
+		}
+
+		TEST_METHOD(TestSolveNQueens)
+		{
+			int n = 4;
+
+			// Захоплення вихідних даних
+			ostringstream outputBuffer;
+			streambuf* oldCoutBuffer = cout.rdbuf(outputBuffer.rdbuf());
+
+			solveNQueens(0, n);
+
+			cout.rdbuf(oldCoutBuffer); // Відновлення початкового буфера
+
+			// Очікуваний вивід для n=4 (всі можливі рішення)
+			string expectedOutput =
+				". Q . . \n"
+				". . . Q \n"
+				"Q . . . \n"
+				". . Q . \n\n"
+
+				". . Q . \n"
+				"Q . . . \n"
+				". . . Q \n"
+				". Q . . \n\n";
+
+			Assert::AreEqual(expectedOutput, outputBuffer.str());
+		}
+	};
+}
