c09228ae24f19f62791da22a9c4d5ebec057a2e0
Mr_ShadowKnight_ <volodymyr.pinchak@gmail.com>
2024-11-20 22:49:17 +0200

    Add.index

---
+++ Lab-3/Lab-3/Lab-3.cpp
@@ -0,0 +1,65 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+// Функція для обчислення факторіалу
+long long factorial(int x) {
+    long long result = 1;
+    for (int i = 1; i <= x; ++i) {
+        result *= i;
+    }
+    return result;
+}
+
+void calculateStirlingAndBell(int n) {
+    vector<vector<long long>> stirling(n + 1, vector<long long>(n + 1, 0));
+    vector<long long> bell(n + 1, 0);
+
+    // Початкові значення
+    stirling[0][0] = 1;
+    for (int i = 1; i <= n; ++i) {
+        stirling[i][0] = 0;
+        stirling[i][i] = 1;
+    }
+
+    // Обчислення чисел Стірлінга
+    for (int i = 2; i <= n; ++i) {
+        for (int k = 1; k < i; ++k) {
+            stirling[i][k] = stirling[i - 1][k - 1] + k * stirling[i - 1][k];
+        }
+    }
+
+    // Обчислення чисел Белла
+    for (int i = 1; i <= n; ++i) {
+        for (int k = 1; k <= i; ++k) {
+            bell[i] += stirling[i][k];
+        }
+    }
+
+    // Виведення таблиці
+    cout << "Таблиця чисел Стірлінга другого роду:" << endl;
+    for (int i = 1; i <= n; ++i) {
+        for (int k = 1; k <= i; ++k) {
+            cout << stirling[i][k] << "\t";
+        }
+        cout << endl;
+    }
+
+    cout << "Числа Белла:" << endl;
+    for (int i = 1; i <= n; ++i) {
+        cout << "B(" << i << ") = " << bell[i] << endl;
+    }
+}
+// Основна функція
+int main() {
+    int n = 10, r = 8;
+    long long arrangements = factorial(n) / factorial(n - r);
+
+    cout << "Кількість розміщень без повторень з " << n << " елементів по " << r << ": " << arrangements << endl;
+    int variant = 10;
+    int v = variant + 5; // n = і + 5, де і - номер варіанту
+
+    calculateStirlingAndBell(v);
+    return 0;
+}
---
+++ Lab-3/UnitTest1/UnitTest1.cpp
@@ -0,0 +1,60 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Lab-3/Lab-3.cpp" // замініть на назву вашого заголовного файлу
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest
+{
+    TEST_CLASS(UnitTest)
+    {
+    public:
+
+        // Тест функції обчислення факторіалу
+        TEST_METHOD(TestFactorial)
+        {
+            Assert::AreEqual(1LL, factorial(0));  // 0! = 1
+            Assert::AreEqual(1LL, factorial(1));  // 1! = 1
+            Assert::AreEqual(120LL, factorial(5)); // 5! = 120
+        }
+
+        // Тест розміщень
+        TEST_METHOD(TestArrangements)
+        {
+            int n = 10, r = 8;
+            long long expected = factorial(n) / factorial(n - r);
+            Assert::AreEqual(expected, factorial(10) / factorial(2));
+        }
+
+        // Тест чисел Белла
+        TEST_METHOD(TestBellNumbers)
+        {
+            int n = 5;
+            vector<vector<long long>> stirling(n + 1, vector<long long>(n + 1, 0));
+            vector<long long> bell(n + 1, 0);
+
+            stirling[0][0] = 1;
+            for (int i = 1; i <= n; ++i) {
+                stirling[i][0] = 0;
+                stirling[i][i] = 1;
+            }
+
+            for (int i = 2; i <= n; ++i) {
+                for (int k = 1; k < i; ++k) {
+                    stirling[i][k] = stirling[i - 1][k - 1] + k * stirling[i - 1][k];
+                }
+            }
+
+            for (int i = 1; i <= n; ++i) {
+                for (int k = 1; k <= i; ++k) {
+                    bell[i] += stirling[i][k];
+                }
+            }
+
+            // Перевірка відомих значень чисел Белла
+            Assert::AreEqual(1LL, bell[1]);
+            Assert::AreEqual(15LL, bell[4]);
+            Assert::AreEqual(52LL, bell[5]);
+        }
+    };
+}
