124200ad3ed30f162197c0bcc438d83c4c5cc07a
Mr_ShadowKnight_ <volodymyr.pinchak@gmail.com>
2024-11-20 22:37:53 +0200

    Add.index

---
+++ Lab-2/UnitTest1/UnitTest1.cpp
@@ -0,0 +1,23 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Lab-2/Lab-2.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTestTruthTable
+{
+    TEST_CLASS(UnitTestTruthTable)
+    {
+    public:
+
+        // Функція для тестування обчислення p → q
+        TEST_METHOD(TestImplies)
+        {
+            // Тестуємо операцію p → q
+            Assert::IsTrue(implies(false, false)); 
+            Assert::IsTrue(implies(false, true));
+            Assert::IsTrue(implies(true, true));   
+            Assert::IsFalse(implies(true, false)); 
+        }
+    };
+}
